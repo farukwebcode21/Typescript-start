0524dfe131206371e393bf9b3b1b4ea963510fa8
farukwebcode21 <farukahmadmk@yahoo.com>
2023-05-03 23:27:48 +0600

    learn interface complete

--- src/module_3/generic.ts
+++ src/module_3/generic.ts
@@ -1,11 +1,21 @@
-type generaicArray<T> = Array<T>;
+// type generaicArray<T> = Array<T>;
 
-const rollNumber1: generaicArray<string> = ["Faruk", "Rakib", "Rustom"];
-const rollNumber2: Array<number> = [4, 22, 30];
-const rollNumber3: Array<boolean> = [true, false];
-console.log(rollNumber1);
-console.log(rollNumber2);
-console.log(rollNumber3);
+
+
+// const rollNumber1: generaicArray<string> = ["Faruk", "Rakib", "Rustom"];
+// const rollNumber2: Array<number> = [4, 22, 30];
+// const rollNumber3: Array<boolean> = [true, false];
+// console.log(rollNumber1);
+// console.log(rollNumber2);
+// console.log(rollNumber3);
+
+function getArray<T>(items:T[]):T[]{
+  return new Array<T>().concat(items);
+}
+
+let myNumber = getArray<number>([100, 200, 300]);
+let myStrArr = getArray< string>(["Hello", "World", "Bangaleesh"]);
+console.log(myStrArr);
 
 function identity(arg: number): number {
   return arg;
--- src/module_3/interface.ts
+++ src/module_3/interface.ts
@@ -1,25 +1,50 @@
-
+// Interface Example ===========
 interface IUser {
-    id:number;
-    name:string;
-    age:number;
+  id: number;
+  name: string;
+  age: number;
 }
 
-let users:IUser[] =[];
-
-let user1:IUser={
-    id:1,
-    name:"Faruk",
-    age:31,
+// Extend interface
+interface IExtendUser extends IUser {
+  role: string;
+  country: string;
+  developer: boolean;
 }
 
-let user2:IUser={
-    id:2,
-    name:"Faruk Ahmad",
-    age:30,
-}
+let user1: IUser = {
+  id: 1,
+  name: "Faruk",
+  age: 31,
+};
+
+let user2: IUser = {
+  id: 2,
+  name: "Faruk Ahmad",
+  age: 30,
+};
+
+let user3: IExtendUser = {
+  id: 3,
+  name: "Rashel Khan",
+  age: 25,
+  role: "Admin",
+  country: "Banglaesh",
+  developer: true,
+};
+let users: IUser[] = [];
 
 users.push(user1);
 users.push(user2);
+users.push(user3);
 
-console.log(users);//[{ id: 1, name: 'Faruk', age: 31 },  { id: 2, name: 'Faruk Ahmad', age: 30 }]
+console.log(users); //[{ id: 1, name: 'Faruk', age: 31 },  { id: 2, name: 'Faruk Ahmad', age: 30 }]
+
+interface IRollNumbers {
+  [index: number]: number;
+}
+ const arr:number[] =[1, 2, 3, 5];
+ arr.push(50,90, 100, 45,36);
+ arr.push(60);
+ arr.push(80);
+ console.log(arr);
