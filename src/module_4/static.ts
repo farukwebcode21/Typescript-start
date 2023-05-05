eb0739806df7c50bf604e64893c04c31546d83cf
farukwebcode21 <farukahmadmk@yahoo.com>
2023-05-05 10:20:19 +0600

    example static type typescript

--- src/module_4/static.ts
+++ src/module_4/static.ts
@@ -1,18 +1,19 @@
 class Counter {
-    counter:number;
-    constructor(counter:number){
-        this.counter = counter;
-    }
-    increment(){
-        return (this.counter =this.counter +1);
-    }
-    decrement(){
-        return (this.counter = this.counter -1)
-    }
+  static counter: number = 0;
+
+  static increment() {
+    return (Counter.counter = Counter.counter + 1);
+  }
+  static decrement() {
+    return (Counter.counter = Counter.counter - 1);
+  }
 }
 
-const instance1 = new Counter(0);
-const instance2 = new Counter(1);
+// const instance1 = new Counter();
+// const instance2 = new Counter();
 
-console.log(instance1.increment());
-console.log(instance2.decrement());
+console.log(Counter.increment());
+console.log(Counter.increment());
+
+//Decrement instance2
+console.log(Counter.decrement());
